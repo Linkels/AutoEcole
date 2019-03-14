2b27efe70f9ca33af3815b77df87e478ebca1360
LInkels <m.chaumoitre@simplon-charleville.fr>
2019-03-14 16:27:29 +0100

    1.0

---
+++ code.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+     <meta name="viewport" content="width=device-width"/>
+      <script src="scripts/main.js"></script>
+       <link href="styles/style.css" rel="stylesheet" type="text/css">
+        <meta charset="UTF-8">
+         <title>PERMIS B</title>
+          </head>
+
+<?php include "header.php";?>
+
+<body></body>
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+<?php include "footer.php";?>
+
+</html>
---
+++ header.php
@@ -0,0 +1,31 @@
+<header>
+  <p class="flotte">
+    <a href="index.php">
+    <img src="image/logo-280px-.jpg" width=150 /></a>
+      <center> <div id="name">AUTO-ECOLE PILOT'IN</div> </center>
+        <div id="searchbar">
+          <form action="" class="formulaire">
+            <input class="champ" type="text" value="" required placeholder="Recherche" />
+              <input class="bouton" type="button" value=" " />
+                 </form>
+                   </div
+                     </p>
+                       <div id="menu">
+                         <ul>
+                          <li><a class="click" href="tarifs.php">Tarifs</a>
+                           <ul>
+                            <li><a class="click" href="a.php">Permis A</a></li>
+                            <li><a class="click" href="b.php">Permis B</a></li>
+                              </ul>
+                               </li>
+                                <li><a class="click" href="code.php">Code en ligne</a></li>
+                                <li><a class="click" href="calendrier.php">Calendrier</a></li>
+                                <li><a class="click" href="propos.php">A propos</a>
+                                    <ul>
+                                     <li><a class="click" href="who.php">Qui sommes-nous ?</a></li>
+                                     <li><a class="click" href="contacter.php">Contact</a></li>
+                                        </ul>
+                                         </li>
+                                          </ul>
+                                           </div>
+                       </header>

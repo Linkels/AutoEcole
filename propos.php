40c0886c4a09c5a564358cff6c5d310669815bc1
LInkels <m.chaumoitre@simplon-charleville.fr>
2019-03-15 08:51:15 +0100

    1.0.1

--- propos.php
+++ propos.php
@@ -4,6 +4,7 @@
 
   <head>
      <meta name="viewport" content="width=device-width"/>
+     <link rel="shortcut icon" href="image/logo-280px-.ico">
       <script src="scripts/main.js"></script>
        <link href="styles/style.css" rel="stylesheet" type="text/css">
         <meta charset="UTF-8">

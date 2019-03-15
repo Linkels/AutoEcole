6a167d1479386a733e54f1baeb45fc8e5f4ab146
MoiAngie <a.maas@simplon-charleville.fr>
2019-03-15 16:23:04 +0100

    dernier essai pour ajdhui

--- emploi.php
+++ emploi.php
@@ -13,10 +13,43 @@
 
 <?php include "header.php";?>
 
-<body></body>
+<body>
 
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+    <div id="bloc-gauche">
+      <h2 class="pd20">Offres d'emploi</h2>
+      <p class="pd20 fs10">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+
+  </div>
+
+  <div id="bloc-droit">
+      <h2 class="pd20">Informations</h2>
+      <h3 class="pd20">Comment devenir Moniteur(ice)?</h3>
+      <p class="pd20 fs10">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+
+  </div>
+
+
+
+
+
+</body>
+
+
 
 <?php include "footer.php";?>
 
